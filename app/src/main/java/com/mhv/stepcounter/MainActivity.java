78ae9afd6ed2b16d8fc09c62714d676de659a579
MHV1 <mhv1@mhv.com>
2016-03-16 14:24:03 +0200

    Initial commit

---
+++ app/src/main/java/com/mhv/stepcounter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mhv.stepcounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Content displayed as Fragment from MainActivityFragment class.
+    }
+}
